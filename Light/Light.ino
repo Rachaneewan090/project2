b7604bd7b09ae6de47baa66f21f6feff67aca428
Rachaneewan090 <racha_ning@hotmail.com>
2017-12-07 22:31:49 +0700

    light

---
+++ Light/Light.ino
@@ -0,0 +1,15 @@
+#define PIN A0
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int light = analogRead(PIN);
+  Serial.println(light);
+  delay(10000);
+
+}

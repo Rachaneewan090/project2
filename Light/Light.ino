ef0429bb0fcb3c07315928d9d69d53f0b9b58530
Rachaneewan090 <racha_ning@hotmail.com>
2017-12-10 14:32:09 +0700

    connect wifi

--- Light/Light.ino
+++ Light/Light.ino
@@ -1,9 +1,25 @@
 #define PIN A0
+#include <ESP8266WiFi.h>
+
+//setup wifi
+const char* ssid     = "PAINAIMA";
+const char* password = "0805205425";
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 
+  //Connect wifi
+  Serial.println("Starting...");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) 
+    {
+        delay(250);
+        Serial.print(".");
+     }
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
--- TEMPnHUMI/TEMPnHUMI.ino
+++ TEMPnHUMI/TEMPnHUMI.ino
@@ -1,16 +1,33 @@
-
 #include "DHT.h"
 #include <Adafruit_Sensor.h>
+#include <ESP8266WiFi.h>
 
 #define DHTPIN D4 // what pin we're connected to
 #define DHTTYPE DHT22 // DHT 11 
+
+//setup wifi
+const char* ssid     = "PAINAIMA";
+const char* password = "0805205425";
 
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
 Serial.begin(9600); 
+//DHT
 Serial.println("DHTxx test!");
 dht.begin();
+
+//Connect wifi
+Serial.println("Starting...");
+WiFi.begin(ssid, password);
+while (WiFi.status() != WL_CONNECTED) 
+  {
+      delay(250);
+      Serial.print(".");
+   }
+Serial.println("WiFi connected");  
+Serial.println("IP address: ");
+Serial.println(WiFi.localIP());
 }
 
 void loop() {

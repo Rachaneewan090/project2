224cf6845c3ce445cfbe2228abff00a6e129d4d3
Rachaneewan090 <racha_ning@hotmail.com>
2017-12-11 13:28:40 +0700

    connect to microgear

--- TEMPnHUMI/TEMPnHUMI.ino
+++ TEMPnHUMI/TEMPnHUMI.ino
@@ -1,40 +1,84 @@
-#include "DHT.h"
-#include <Adafruit_Sensor.h>
+#include <MicroGear.h>
 #include <ESP8266WiFi.h>
+#include "DHT.h"
 
-#define DHTPIN D4 // what pin we're connected to
-#define DHTTYPE DHT22 // DHT 11 
-
-//setup wifi
 const char* ssid     = "PAINAIMA";
 const char* password = "0805205425";
 
+#define APPID   "DemoKeppa"
+#define KEY     "MiMqaX43JNzWoux"
+#define SECRET  "wizMipeH7aoZydvLbOVGcueai"
+
+#define ALIAS   "DHT22"
+#define TargetWeb "HTML_web"
+
+#define D4 2   // TXD1
+#define DHTPIN D4     // what digital pin we're connected to
+#define DHTTYPE DHT22   // DHT 11
+
 DHT dht(DHTPIN, DHTTYPE);
 
-void setup() {
-Serial.begin(9600); 
-//DHT
-Serial.println("DHTxx test!");
-dht.begin();
+WiFiClient client;
+MicroGear microgear(client);
 
-//Connect wifi
-Serial.println("Starting...");
-WiFi.begin(ssid, password);
-while (WiFi.status() != WL_CONNECTED) 
-  {
-      delay(250);
-      Serial.print(".");
-   }
-Serial.println("WiFi connected");  
-Serial.println("IP address: ");
-Serial.println(WiFi.localIP());
+void onMsghandler(char *topic, uint8_t* msg, unsigned int msglen) 
+{
+    Serial.print("Incoming message --> ");
+    msg[msglen] = '\0';
+    Serial.println((char *)msg);
+}//เป็นการพิมพ์ข้อความที่ได้รับออกทางพอร์ต Serial
+
+void onConnected(char *attribute, uint8_t* msg, unsigned int msglen) 
+{
+    Serial.println("Connected to NETPIE...");
+    microgear.setAlias(ALIAS);
+}//เป็นการพิมพ์สถานะการเชื่อมต่อสำเร็จไปยังพอร์ต Serial และตั้งชื่อเรียกให้กับอุปกรณ์
+
+void setup() 
+{
+     /* Event listener */
+    microgear.on(MESSAGE,onMsghandler);
+    microgear.on(CONNECTED,onConnected);
+
+    dht.begin();
+    Serial.begin(115200);
+    Serial.println("Starting...");
+
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) 
+    {
+       delay(250);
+       Serial.print(".");
+    }
+
+    Serial.println("WiFi connected");  
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    microgear.init(KEY,SECRET,ALIAS);//สั่งให้ Microgear ต่อกับ NETPIE
+    microgear.connect(APPID);
 }
 
-void loop() {
-  Serial.println("why not show");
-   int H = dht.readHumidity();
-   Serial.println(H);
-   int T = dht.readTemperature();
-   Serial.println(T);
-   delay(5000);
+void loop() 
+{
+    if (microgear.connected())
+    {
+       microgear.loop();
+       Serial.println("connected");
+
+       float Humidity = dht.readHumidity();
+       float Temp = dht.readTemperature();  // Read temperature as Celsius (the default)
+       String data = "/" + String(Humidity) + "/" + String(Temp);
+       char msg[128];
+       data.toCharArray(msg,data.length());
+       Serial.println(msg);    
+
+       microgear.chat(TargetWeb , msg);
+    }
+   else 
+   {
+    Serial.println("connection lost, reconnect...");
+    microgear.connect(APPID);
+   }
+    delay(1500);
 }
